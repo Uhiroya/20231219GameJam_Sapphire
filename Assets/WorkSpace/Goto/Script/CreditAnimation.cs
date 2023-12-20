8389deb9a5bea14fc3c4bf75c3aa63f2110dbf25
原島 由和 <harashima_yoshikazu@cyberagent.co.jp>
2023-12-20 16:22:04 +0900

    マージした

--- Assets/WorkSpace/Goto/Script/CreditAnimation.cs
+++ Assets/WorkSpace/Goto/Script/CreditAnimation.cs
@@ -1,18 +1,15 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class CreditAnimation : MonoBehaviour
 {
-    [SerializeField]
-    CanvasGroup _winCanvasGroup;
-    [SerializeField]
-    RectTransform rectTransform;
-    [SerializeField]
-    float _endPositionY = 0;
-    [SerializeField]
-    float _whitePanelAnimationTime = 0;
-    [SerializeField]
-    float _animationTime = 1f;
+    [SerializeField] CanvasGroup _winCanvasGroup;
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] float _endPositionY = 0;
+    [SerializeField] float _whitePanelAnimationTime = 0;
+    [SerializeField] float _animationTime = 1f;
 
     private void Start()
     {
@@ -24,8 +21,10 @@ public class CreditAnimation : MonoBehaviour
     public void StartCreditAnimation()
     {
         _winCanvasGroup.DOFade(1.0f, _whitePanelAnimationTime).OnComplete(() =>
-        rectTransform.DOAnchorPos(new Vector2(0, _endPositionY), _animationTime)
+            rectTransform.DOAnchorPos(new Vector2(0, _endPositionY), _animationTime).OnComplete(() =>
+            {
+                SceneManager.LoadScene("Title");
+            })
         );
-
     }
 }
