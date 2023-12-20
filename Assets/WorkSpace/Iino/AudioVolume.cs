12100347a9940c2e8d35eca39dab96fc4efba03b
syun39 <134475243+syun39@users.noreply.github.com>
2023-12-20 15:04:14 +0900

    [add]音の追加

---
+++ Assets/WorkSpace/Iino/AudioVolume.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class AudioVolume : MonoBehaviour
+{
+    public void SetAudioVolumeAdjustment()
+    {
+        var audioSource = this.gameObject.GetComponent<AudioSource>();
+        audioSource.DOFade(
+            0.0f,   // 音量
+            5.0f    // 段々と小さくなっていく時間（秒)
+        ).OnStart(() =>
+        {
+            audioSource.volume = 1.0f;
+        });
+    }
+
+    public void Start()
+    {
+        SetAudioVolumeAdjustment();
+    }
+}
