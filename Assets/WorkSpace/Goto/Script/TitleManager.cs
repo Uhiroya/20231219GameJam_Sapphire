1533909941b93a8d06f09eb1d46341d14fb9f708
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-12-20 15:38:19 +0900

    [add]タイトルシーンの追加

---
+++ Assets/WorkSpace/Goto/Script/TitleManager.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField]
+    CanvasGroup _startCanvasGroup;
+    [SerializeField]
+    float _imageFadeTime = 1f;
+
+    private void Start()
+    {
+        _startCanvasGroup.alpha = 0;
+        _startCanvasGroup.DOFade(1.0f, _imageFadeTime).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+#if UNITY_EDITOR
+            Debug.Log("GO InGameScene");
+#endif
+            //SceneManager.LoadScene("InGame");
+        }
+    }
+}
