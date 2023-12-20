f56298ef59d4ab27ce2621b80af5c41fb5020ccf
原島 由和 <harashima_yoshikazu@cyberagent.co.jp>
2023-12-20 10:18:19 +0900

    マップ周りの修正

--- Assets/Harashima/Scripts/MapController.cs
+++ Assets/Harashima/Scripts/MapController.cs
@@ -7,16 +7,25 @@ using UnityEngine.UI;
 public class MapController : MonoBehaviour
 {
     [SerializeField] private Transform _mapParent;
-    [SerializeField] private Image _bulletImage;
-    [SerializeField] private Image _enemyImage;
+    [SerializeField] private Image _bulletImagePrefab;
+    [SerializeField] private Image _enemyImagePrefab;
 
     [SerializeField] private Transform _enemy;
     [SerializeField] private Transform _player;
 
+    [SerializeField, Header("マップのサイズ")] private float _mapSize = 10f;
+
+    private Image _enemyImage;
     private void Start()
     {
-        var dir = _enemy.position * 3.5f;
-        var ui = Instantiate(_bulletImage, _mapParent);
-        ui.rectTransform.localPosition = new Vector3(dir.z,dir.x, 0);
+        _enemyImage = Instantiate(_enemyImagePrefab, _mapParent);
+    }
+
+    private void Update()
+    {
+        _mapParent.rotation = Quaternion.Euler(0, 0, _player.eulerAngles.y);
+        var pos = (_enemy.position - _player.position) / _mapSize * 100f;
+        _enemyImage.rectTransform.anchoredPosition = new Vector3(pos.x,pos.z, 0);
+
     }
 }
